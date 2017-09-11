676f9d07a89382ce9523c765dd2210524f8af084
Jebb Domingo <jebb@timble.net>
2017-09-11 09:20:43 +0800

    Fix installation of ckeditor

--- code/administrator/components/com_rewardlabs/resources/install/script.php
+++ code/administrator/components/com_rewardlabs/resources/install/script.php
@@ -11,4 +11,35 @@ require_once __DIR__.'/helper.php';
 
 class com_rewardlabsInstallerScript extends JoomlatoolsInstallerHelper
 {
+    public function afterInstall($type, $installer)
+    {
+        if ($type !== 'discover_install') {
+            $source = $installer->getParent()->getPath('source').'/extensions/ckeditor';
+            $map = array(
+                $source                     => JPATH_ROOT.'/libraries/joomlatools-components/ckeditor',
+                $source.'/resources/assets' => JPATH_ROOT.'/media/koowa/com_ckeditor'
+            );
+
+            foreach ($map as $from => $to)
+            {
+                $temp   = $to.'_tmp';
+
+                if (!JFolder::exists($from)) {
+                    continue;
+                }
+
+                if (JFolder::exists($temp)) {
+                    JFolder::delete($temp);
+                }
+
+                JFolder::copy($from, $temp);
+
+                if (JFolder::exists($to)) {
+                    JFolder::delete($to);
+                }
+
+                JFolder::move($temp, $to);
+            }
+        }
+    }
 }
